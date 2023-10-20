601e793f0e42a4624580c6eaea8f870d9548f1e3
wowdinow <f.wadino@gmail.com>
2023-10-20 14:51:42 +0700

    Initialization User Feature

---
+++ controllers/userController.js
@@ -0,0 +1,67 @@
+class UserController {
+    static async login(req, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async register(req, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async resetPassword(res, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async getUser(req, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async updateProfile(req, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async addMyPlant(req, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async updateMyPlant(req, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+
+    static async removePlant(req, res, next){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = UserController
