1fedf9891a06b9a9baf79ca41360f8c863cc19ca
Legawa4201 <LegawaP12@gmail.com>
2023-10-23 16:44:01 +0700

    unit testing

---
+++ helpers/socket.js
@@ -0,0 +1,8 @@
+const { io } = require(`../bin/www`)
+
+// io.on(`connection`, (socket) => {
+//     console.log(`A user connected with id ${socket.id}`)
+// })
+// // io.on(`connection`, (socket) => {
+// //     console.log(`A user connected with id ${socket.id}`)
+// // })
